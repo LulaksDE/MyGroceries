aa0515bbe403daca46a1ba080a58d738ab656577
Lulaks <lukasmaruszczyk@web.de>
2024-10-16 19:35:58 +0200

    Top Bar implementation and Dropdown menu

--- app/src/main/java/com/lulakssoft/mygroceries/MainActivity.kt
+++ app/src/main/java/com/lulakssoft/mygroceries/MainActivity.kt
@@ -4,13 +4,10 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import com.lulakssoft.mygroceries.ui.theme.MyGroceriesTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,29 +16,21 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             MyGroceriesTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Samuel",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                MyGroceriesApp()
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    MyGroceriesTheme {
-        Greeting("Android")
+fun MyGroceriesApp() {
+    val navController = rememberNavController()
+    NavHost(navController = navController, startDestination = "mainView") {
+        composable(route = "mainView") {
+            MainView(navController = navController)
+        }
+        composable(route = "secondView") {
+            SecondView()
+        }
     }
 }
---
+++ app/src/main/java/com/lulakssoft/mygroceries/MainView.kt
@@ -0,0 +1,112 @@
+package com.lulakssoft.mygroceries
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedButton
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+import org.w3c.dom.Text
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun MainView(navController: NavController) {
+    val expanded = remember { mutableStateOf(false) }
+    val selectedOption = remember { mutableStateOf("My Groceries App") }
+
+    Scaffold(topBar = {
+        TopAppBar(
+            title = {
+                Row(
+                    modifier = Modifier.fillMaxWidth(),
+                    verticalAlignment = Alignment.CenterVertically,
+                    horizontalArrangement = Arrangement.Center,
+                ) {
+                    Text(
+                        selectedOption.value,
+                        fontWeight = FontWeight.Bold,
+                        modifier = Modifier.clickable { expanded.value = true },
+                    )
+                    IconButton(onClick = { expanded.value = true }) {
+                        Icon(
+                            painter = painterResource(id = R.drawable.baseline_arrow_drop_down_24),
+                            contentDescription = "Dropdown Menu",
+                        )
+                    }
+                }
+            },
+            modifier = Modifier.fillMaxWidth(),
+        )
+        DropdownMenu(
+            expanded = expanded.value,
+            onDismissRequest = { expanded.value = false },
+        ) {
+            DropdownMenuItem(
+                text = { Text("Haushalt 01") },
+                onClick = {
+                    selectedOption.value = "Haushalt 01"
+                    expanded.value = false
+                },
+            )
+            DropdownMenuItem(
+                text = { Text("Haushalt 02") },
+                onClick = {
+                    selectedOption.value = "Haushalt 02"
+                    expanded.value = false
+                },
+            )
+        }
+    }) { innerPadding ->
+        Column(
+            modifier =
+                Modifier
+                    .padding(innerPadding)
+                    .fillMaxHeight(),
+            horizontalAlignment = Alignment.CenterHorizontally,
+        ) {
+            Content(navController)
+        }
+    }
+}
+
+@Composable
+fun Content(navController: NavController) {
+    Row(
+        modifier =
+            Modifier
+                .clip(shape = RoundedCornerShape(10.dp))
+                .background(MaterialTheme.colorScheme.primary)
+                .padding(25.dp),
+    ) {
+        Column(Modifier.weight(1f)) {
+            Text("Dont forget to buy groceries!", fontWeight = FontWeight.Bold, color = MaterialTheme.colorScheme.onPrimary)
+        }
+        OutlinedButton(
+            onClick = { navController.navigate("secondView") },
+            modifier = Modifier.padding(top = 2.dp),
+        ) { Text("Haushalt", color = MaterialTheme.colorScheme.onPrimary) }
+    }
+}
